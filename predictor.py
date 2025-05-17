5ded62839bae0918cfccca48f0b1a9719d1c8229
Sethita911 <99404311+Sethita911@users.noreply.github.com>
2025-05-17 15:33:06 +0100

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 def generate_prediction():
-    url = "https://github.com/Sethita911/football-prediction-bot./blob/main/predictions.csv"
+    url = "predictions.csv"
     df = pd.read_csv(url)
     return df
 
